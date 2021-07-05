feefab108c5a4afbe83970f44b5c0d7a7912c2e3
yeasellllllllll <yeasel6112@gmail.com>
2021-07-05 14:57:58 +0900

    issue #1

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
 
-print("Hello Bioinformatics")
+import sys
+
+name = sys.argv[1] #스페이스를 기준으로 인자들이 list로 구분되는 것
+name2 = sys.argv[2]
+
+print(f"Hello {name}")
+print(f"Hello {name2}")
 
